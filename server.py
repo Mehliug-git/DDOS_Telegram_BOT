2b13cb1022162b63f0a44fbac0a4812906aa8c0e
Mehliug <guilhemferreira6@gmail.com>
2023-04-12 18:22:47 +0200

    Update server.py

--- server.py
+++ server.py
@@ -11,8 +11,15 @@ bot_process = subprocess.Popen(['python3' , 'bot.py'])
 @app.route("/")
 def page_web_de_mort():
     bot_process
-    return "<html><body><img src='https://i.giphy.com/3o7aTskHEUdgCQAXde.gif'></body></html>"
+    return "<html><body><img src='https://i.giphy.com/KmHueA88mFABT9GkkR.gif'></body></html>"
     #page_web_de_mort()
+    
+@app.route("/kill")
+def killswitch():
+    p = subprocess.Popen(f'refresh', stdout=subprocess.PIPE, shell=True)
+    output, error = p.communicate()
+    bot_process
+    return "<html><body><img src='https://i.giphy.com/6jTD6KqVNADI746GOC.gif'></body></html>"     
     
 
 app.run()
