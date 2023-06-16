84fe09a1fffa825695b10e5e0f44505eb7b1a630
Mehliug <78538454+Mehliug-git@users.noreply.github.com>
2023-06-16 18:12:48 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
 from flask import Flask, render_template
 import subprocess
-
+import os 
+import signal
 
 app = Flask(__name__)
 
 bot_process = subprocess.Popen(['python3' , 'bot.py'])
+
+
+def kill_subprocesses():
+    current_pid = os.getpid()
+    cmd = f'pgrep -P {current_pid}'
+    pids = subprocess.check_output(cmd, shell=True).decode().split()
+    for pid in pids:
+        try:
+            os.kill(int(pid), signal.SIGTERM)
+        except OSError:
+            pass
+
 
 
 
@@ -16,6 +29,7 @@ def page_web_de_mort():
     
 @app.route("/kill")
 def killswitch():
+    kill_subprocesses()
     p = subprocess.Popen(f'refresh', stdout=subprocess.PIPE, shell=True)
     output, error = p.communicate()
     bot_process
