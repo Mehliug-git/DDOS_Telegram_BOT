d043a83e4173e60ec6f700fec142d8c51c88a39c
MODxSMG123456 <sellkar121@gmail.com>
2024-07-02 20:35:34 +0530

    bot.py

--- bot.py
+++ bot.py
@@ -15,8 +15,8 @@ import concurrent.futures
 
 
 #Telegram token
-token = os.getenv('TELEGRAM_TOKEN')
-bot_number = os.getenv('NUM')
+token = os.getenv('7026113454:AAHYu1mv7DlWdPyJVXAINr5Yyq9pTv8gsxI')
+bot_number = os.getenv('1795306025')
 updater = Updater(token,use_context=True)
 s = 360
 
